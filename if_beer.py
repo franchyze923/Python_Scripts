6a4200561d2136fc2b10312ec861d116cf6d1bae
Fran P <fpolig01@villanova.edu>
2015-04-16 11:28:55 -0400

    adding if_beer

---
+++ if_beer.py
@@ -0,0 +1,13 @@
+
+FranBeer = ['bells','victory','troegs','dogfish','sierra nevada']
+
+for x in FranBeer:
+    print(x)
+    if x == 'bells':
+        print("this one is bells ")
+    elif x=='victory':
+        print('this is victory')
+    elif x=='dogfish':
+        print('this is dog')
+    else:
+        print ('this is not bells,vic or dog  ')
