03e46fc1ae5c065ff7a6da1660004ed2a665702e
Fran <fpolig01@villanova.edu>
2017-10-03 15:58:53 -0400

    Adding new FizzBuzz file

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+## FizzBuzz
+
+for x in range(1, 101, 1):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz!')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
+
