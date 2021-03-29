70c20dfe1ca26c4eefd511f5f925eb55e5553df7
Francis Polignano <franchyze923@gmail.com>
2021-03-29 10:32:50 -0400

    Update ffmpeg_video_converter.py
    using handbrake cli instead of ffmpeg. For some reason plex does not like the version I produce with ffmpeg, but handbrake cli works fine.

--- ffmpeg_video_converter.py
+++ ffmpeg_video_converter.py
@@ -11,6 +11,7 @@ root_video_directory = r"E:\Media\Videos"
 root_output_directory = r"D:\Videos"
 ffmpeg_location = r"C:\Users\franp\Downloads\ffmpeg-2021-03-28-git-8b2bde0494-full_build\bin\ffmpeg.exe"
 
+# noinspection PyArgumentList
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s",
                     handlers=[
                         logging.FileHandler(r"D:\export_log.log"),
@@ -27,13 +28,17 @@ for subdir, dirs, files in os.walk(root_video_directory):
 
             Path(os.path.dirname(avi_file).replace('E', 'D', 1)).mkdir(parents=True, exist_ok=True)
             output_file = os.path.join(avi_file.replace('E', 'D', 1).replace(".avi", ".mp4"))
+            ## h.265 did not work with plex direct stream
+            #ffmpeg_command = [ffmpeg_location, '-i', f'{avi_file}',  "-c:v", "libx265", "-crf", "26", output_file]
+            #ffmpeg_command = [ffmpeg_location, '-i', f'{avi_file}', output_file]
+            #ffmpeg_command = [ffmpeg_location, '-i', f'{avi_file}', "-c:v", "libx264", "-crf", "20", output_file]
+            handbrake_command = [r"C:\Users\franp\Downloads\HandBrakeCLI-1.3.3-win-x86_64\HandBrakeCLI.exe", '-i', f'{avi_file}',"-o", output_file, "-e", "x264", "-q", "20", "-B", "160"]
 
-            ffmpeg_command = [ffmpeg_location, '-i', f'{avi_file}',  "-c:v", "libx265", "-crf", "20",  output_file]
 
-            logging.info(f"Converting: {avi_file} to .MP4 with x265. Output MP4: {output_file}")
+            logging.info(f"Converting: {avi_file} to .MP4 with x264. Output MP4: {output_file}")
             start_time = time.time()
 
-            process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+            process = subprocess.Popen(handbrake_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
             for line in process.stdout:
                 print(line)
 
